51cda94dd54922ead17f5e5e15906c62b807e06a
mohamedkhedrforblackpyramid <mohamedkhedrforblackpyramid@users.noreply.github.com>
2026-02-26 13:02:25 +0000

    deploy: 5df1dfa4f807499ba2431846cce7f3345e7062ff

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "498101077" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3723529647" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
